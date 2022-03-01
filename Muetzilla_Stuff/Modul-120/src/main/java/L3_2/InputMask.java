1b5b984e0c2b79fa7bde26d88abc85bd45252937
Muetzilla <muetz-github@gasbichler.ch>
2022-03-01 14:56:23 +0100

    ohhoo Top and Bottom working

--- Muetzilla_Stuff/Modul-120/src/main/java/L3_2/InputMask.java
+++ Muetzilla_Stuff/Modul-120/src/main/java/L3_2/InputMask.java
@@ -1,6 +1,7 @@
 package L3_2;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class InputMask extends JFrame {
     private JLabel personTitleLabel =  new JLabel("Person:");
@@ -12,21 +13,50 @@ public class InputMask extends JFrame {
     private JLabel kontakteTitleLabel =  new JLabel("Kontakte:");
     private JLabel telLabel =  new JLabel("Tel");
     private JLabel mailLabel =  new JLabel("Mail");
-    private JLabel imageLabel =  new JLabel(new ImageIcon("src\\main\\java\\L3_2\\  Logo.gif"));
+    private JLabel imageLabel =  new JLabel(new ImageIcon("src\\main\\java\\L3_2\\Logo.gif"));
+    private JLabel title = new JLabel("Informatik/Technik");
+    private JPanel titlePanel =  new JPanel();
+    private JPanel buttonPanel = new JPanel();
+    private JPanel buttonSoutPanel = new JPanel();
+    private JButton neuButton =  new JButton("Neu");
+    private JButton erfassenButton =  new JButton("Erfassen");
+    private JButton verwerfenButton =  new JButton("Verwerfen");
+    private JButton schliessenButton =  new JButton("Schliessen");
+    private JPanel centerPanel = new JPanel();
     public InputMask(){
         super("Beispiel zu GUI-Design");
-        setSize(400,300);
-        add(imageLabel);
+
         displayContent();
         pack();
         setVisible(true);
     }
 
     private void displayContent() {
+        //Title Panel
+        titlePanel.setLayout(new BorderLayout());
+        titlePanel.add(title, BorderLayout.WEST);
+        titlePanel.add(imageLabel, BorderLayout.EAST);
+        //Center Panel
+        centerPanel.setLayout(new GridLayout(0,1));
 
+
+
+        //Button Panel
+        buttonPanel.setLayout(new GridLayout(1,0));
+        buttonPanel.add(neuButton);
+        buttonPanel.add(erfassenButton);
+        buttonPanel.add(verwerfenButton);
+        buttonPanel.add(schliessenButton);
+        buttonSoutPanel.setLayout(new BorderLayout());
+        buttonSoutPanel.add(buttonPanel, BorderLayout.EAST);
+        //Add Panels to main scene
+        add(titlePanel, BorderLayout.NORTH);
+        add(centerPanel, BorderLayout.CENTER);
+        add(buttonSoutPanel, BorderLayout.SOUTH);
     }
 
     public static void main(String[] args) {
-        new InputMask();
+      InputMask im = new InputMask();
+      im.setSize(400,300);
     }
 }
