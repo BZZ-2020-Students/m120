55fa73368989be76c7fd212ed18ec0e1b1f8f379
Zwazel <zwazel@hotmail.com>
2022-04-04 10:31:59 +0200

    die andere 2 files sind jetzt au formatiert

--- nadina_stuff/src/Snake/GameFrame.java
+++ nadina_stuff/src/Snake/GameFrame.java
@@ -1,18 +1,15 @@
 package Snake;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
-public class GameFrame extends JFrame{
-	 
-		GameFrame(){
-
-			this.add(new GamePanel());
-			this.setTitle("Snake");
-			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			this.setResizable(false);
-			this.pack();
-			this.setVisible(true);
-			this.setLocationRelativeTo(null); //set Location in the middle from the window
-			
-		}
+public class GameFrame extends JFrame {
+    GameFrame() {
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null); //set Location in the middle from the window
+    }
 }
--- nadina_stuff/src/Snake/SnakeGame.java
+++ nadina_stuff/src/Snake/SnakeGame.java
@@ -1,10 +1,7 @@
 package Snake;
 
 public class SnakeGame {
-	
-	public static void main(String[] args) {
-		
-		new GameFrame();
-	}
-
+    public static void main(String[] args) {
+        new GameFrame();
+    }
 }
