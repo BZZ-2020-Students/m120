3a8f873e4a9990e25620d495f4fa8142c7df7b13
Zwazel <zwazel@hotmail.com>
2022-04-12 14:09:12 +0200

    erste page

---
+++ tim_stuff/lb02/src/main/java/App.java
@@ -0,0 +1,13 @@
+/**
+ * @author Tim Irmler
+ * <p>
+ * This is the main class for the application.
+ * It just creates a new instance of the DestinationModel class and prints out the destinations.
+ */
+public class App {
+    public static void main(String[] args) {
+        DestinationModel destinationModel = new DestinationModel();
+
+        System.out.println(destinationModel);
+    }
+}
